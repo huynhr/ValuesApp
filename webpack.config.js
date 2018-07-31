a5929f5aa7483c490076f5a5f8a812aee4b95f12
Raymond Huynh <huynh.r.k@gmail.com>
2018-07-30 23:08:31 -0400

    Compile SASS files

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './client/src/index.jsx',
@@ -14,7 +15,38 @@ module.exports = {
         use: {
         loader: "babel-loader"
         }
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract(
+          {
+            fallback: 'style-loader',
+            use: ['css-loader', 'sass-loader']
+          })
       }
     ]
-  }
+  },
+  plugins: [ 
+    new ExtractTextPlugin({filename: 'style.css'})
+  ]
 };
+
+
+
+// {
+//   test: /\.(s*)css$/,
+//   use: [
+//     {
+//       loader: "style-loader"
+//     },
+//     {
+//       loader: "css-loader"
+//     },
+//     {
+//       loader: "sass-loader",
+//       options: {
+//         includesPaths: ["~/client/src", "~/client/dst/style.css"]
+//       }
+//     }
+//   ]      
+// }
