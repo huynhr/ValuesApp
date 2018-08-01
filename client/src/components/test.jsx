c3558bb29a0591182b3667ccd6078891bc8e5c93
Raymond Huynh <huynh.r.k@gmail.com>
2018-07-31 22:31:32 -0400

    Make add auto save and watch to webpack

--- client/src/components/test.jsx
+++ client/src/components/test.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import './test.scss';
 
-const Test = (props) => <h1>Inside of the test</h1>
+const Test = (props) => (
+  <div>
+    <h1>Inside of the test</h1>
+    <h3>Should be red</h3>
+    <p>some jsx here</p>
+    <p>adding some more text</p>
+  </div>
+)
 
 export default Test;
